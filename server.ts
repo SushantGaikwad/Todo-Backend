fb8d12d3c3acfbbd25f255bb461e5bb96600ab6e
Sushant Gaikwad <sushantgaikwad06@gmail.com>
2025-05-09 18:45:14 +0530

    Log added to debug

--- server.ts
+++ server.ts
@@ -1,16 +1,17 @@
-import app from './src/app';
-import connectDB from './src/config/db';
+import app from "./src/app";
+import connectDB from "./src/config/db";
 
 const PORT = process.env.PORT || 5000;
 
-async function startServer()   {
+async function startServer() {
   try {
     await connectDB();
     app.listen(PORT, () => {
+      console.log(">>> Fontend URL :", process.env.FRONTEND_URL);
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
-    console.error('Server failed to start:', error);
+    console.error("Server failed to start:", error);
     process.exit(1);
   }
 }
