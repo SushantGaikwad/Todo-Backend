c0c8d90a36a312b06daf67c57307ab826716a273
Sushant Gaikwad <sushantgaikwad06@gmail.com>
2025-05-09 20:46:29 +0530

    CORS resolved

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173/",
     // credentials: true,
   })
 );
